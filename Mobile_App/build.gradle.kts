6b1df91fe2fb02100112c60330b6da9bbe3a20c5
Mahmooud12 <malbiltagy@gmail.com>
2026-05-08 21:17:28 +0300

    Update build.gradle.kts

--- Mobile_App/build.gradle.kts
+++ Mobile_App/build.gradle.kts
@@ -3,6 +3,5 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 
-    // السطر ده هو اللي هيحل مشكلة الـ Plugin NOT FOUND
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
